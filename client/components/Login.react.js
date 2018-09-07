3460ebd08fb4e9c361f0bad2b2c929babedddad4
Lisa <lisa.cai@outlook.com>
2018-09-07 19:31:12 +0000

    login page works up to the point where it lets you know if you've successfully logged in

--- client/components/Login.react.js
+++ client/components/Login.react.js
@@ -11,27 +11,32 @@ class Login extends React.Component {
     };
   }
     
-onSubmit = async e => {
+  onSubmit = async e => {
     e.preventDefault();
-    const { email, password } = this.state
+    const { email, password } = this.state;
+    
     const validationChecks = {
-    email: validations.isEmail(email),
-    password: validations.isAtLeastLength(password, 8),
+      email: validations.isEmail(email),
+      password: validations.isAtLeastLength(password, 8),
     };
   
-  for (const field of Object.keys(validationChecks)) {
+    for (const field of Object.keys(validationChecks)) {
       if (!validationChecks[field].valid) {
         alert(`${field} ${validationChecks[field].errorMsg}!`);
         return;
       }
-  }
+    }
   
-  const res = await axios.post('/api/user/login', this.state);
+    const res = await axios.post('/api/user/login', this.state);
   
-  if (!res.data.success) {
-      alert('Invalid login');
+    if(res.status == 200){
+      alert('yay you logged in.');
+      
+    }
+      else {
+        alert('invalid login. try again.');
+    }
   }
-}
   
   onChangeField = (field, value) => {
     this.setState({
@@ -47,11 +52,20 @@ onSubmit = async e => {
         <form className="ui form login-form">
           <div className="field">
             <label>Email</label>
-            <input placeholder="Email" />
+            <input 
+              placeholder="Email"
+              value={email}
+              onChange={e => this.onChangeField('email', e.target.value)}
+              />
           </div>
           <div className="field">
             <label>Password</label>
-            <input placeholder="Password" type="password" />
+            <input 
+              placeholder="Password" 
+              type="password"
+              value={password}
+              onChange={e => this.onChangeField('password', e.target.value)}
+              />
           </div>
           <button className="ui button" type="submit" onClick={this.onSubmit}>
             Log In
