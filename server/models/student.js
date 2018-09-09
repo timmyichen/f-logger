8d52c8ecf2463db9c73ac1c1aa51e86e95ea24bd
Tim Chen <timmyichen@users.noreply.github.com>
2018-09-09 03:48:15 -0700

    add students (#32)
    * add student routes
    
    * run prettier

--- server/models/student.js
+++ server/models/student.js
@@ -3,16 +3,23 @@ const uniqueValidator = require('mongoose-unique-validator');
 
 const Schema = mongoose.Schema;
 
-//required props and prop types (a schema defines the shape of the documents within a mongodb collection)
+// required props and prop types (a schema defines the shape of the documents within a mongodb collection)
 const studentSchema = new Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   studentId: { type: Number, required: true, unique: true },
-  isDeleted: { type: Boolean }
+  logs: [
+    {
+      timeOut: Date,
+      timeIn: Date,
+    },
+  ],
+  timeCreated: { type: Date, required: true, default: Date.now },
+  timeDeleted: { type: Date },
 });
 
 studentSchema.plugin(uniqueValidator);
 
-//creating a model (JavaScript object) based on the schema, which has methods to save, update, fetch data by id, etc.
+// creating a model (JavaScript object) based on the schema, which has methods to save, update, fetch data by id, etc.
 module.exports = mongoose.model('Student', studentSchema);
