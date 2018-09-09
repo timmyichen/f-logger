9adf946304b3560d391411b7763a31c707e0bd0f
timmyichen <timmy.i.chen@gmail.com>
2018-09-09 03:40:36 -0700

    add prettier

--- server/models/user.js
+++ server/models/user.js
@@ -4,15 +4,15 @@ const uniqueValidator = require('mongoose-unique-validator');
 const Schema = mongoose.Schema;
 
 //required props and prop types (a schema defines the shape of the documents within a mongodb collection)
-const userSchema = new Schema({ 
+const userSchema = new Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   schoolId: { type: Number },
   email: { type: String, lowercase: true, unique: true, required: true },
   password: { type: String, required: true },
-  isDeleted: { type: Boolean, default: false }
-}); 
+  isDeleted: { type: Boolean, default: false },
+});
 
 userSchema.plugin(uniqueValidator);
 
