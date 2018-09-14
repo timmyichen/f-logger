9b41576aa190be863e57e6628e5b767bede759d9
Jorge Gallardo <jorge8486@gmail.com>
2018-09-14 11:18:00 -0400

    removed unused variables

--- server/routers/students.js
+++ server/routers/students.js
@@ -41,8 +41,6 @@ router.postAsync('/api/students/create', async (req, res) => {
 });
 
 router.deleteAsync('/api/students/delete/:id', async (req, res) => {
-  const [err, students] = await go(Student.find({}));
-
   Student.findOneAndUpdate({studentId: req.params.id}, {$set:{timeDeleted: Date.now()}}, async (err, student) => {
     if (err) return res.status(500).send(err);
     return res.status(200).json({
